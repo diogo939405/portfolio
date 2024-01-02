a3e88e0f4a5648e5981b8e38d2447ae2e91ecbc9
Nilton Cesar Jacintho <nilton.jacintho@gmail.com>
2024-01-02 16:58:30 -0300

    Logo

--- src/Cabeçalho/Cabecalho.js
+++ src/Cabeçalho/Cabecalho.js
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 import './Cabecalho.css'
 import { ToggleP } from './CabecalhoService'
 import { HashLink as Link } from 'react-router-hash-link';
+import logo from "./../imagens/logo_diogo-removebg-preview.png"
 export default function Cabecalho() {
     return (
 
@@ -10,7 +11,9 @@ export default function Cabecalho() {
             <header className='cabecalho'>
                 <div className='menu'>
                     <nav className='navbar'>
-                        <p className='titulo'><span>D</span>iogo Favilla</p>
+                        <p className='titulo'>
+                            <img src={logo} height='60px' width='60px' alt='oie'  ></img>
+                        </p>
                         <ul className='corpo-menu'>
                             <li>
                                 <Link to='#sobre' smooth >
