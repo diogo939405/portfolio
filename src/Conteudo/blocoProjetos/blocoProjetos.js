9c3328bfaf53f971d9b65d7546069af932f07fd6
Nilton Cesar Jacintho <nilton.jacintho@gmail.com>
2024-01-04 15:54:39 -0300

    tela

--- src/Conteudo/blocoProjetos/blocoProjetos.js
+++ src/Conteudo/blocoProjetos/blocoProjetos.js
@@ -14,17 +14,10 @@ export default function BlocoProjetos() {
     // let projeselecionado = -1;
     let getDadosProjetos = getDadosProjeto
     const show = () => {
-        // setProjetoSelecionado(idProjeto);
-        // console.log(getDadosProjetos(projetoSelecionado))
-        let x = document.getElementById('mostrar')
         let d = document.getElementById('descricao')
-        // let t = document.getElementById('titu')
-        // x.showModal()
-        // x.classList.remove('close-dialogo');
-        // x.classList.add('open-dialogo');
-        if(d.style.display === "none"){
-                d.style.display = 'flex'
-        }else{
+        if (d.style.display === "none") {
+            d.style.display = 'flex'
+        } else {
             d.style.display = 'none'
         }
 
@@ -45,42 +38,32 @@ export default function BlocoProjetos() {
 
     return (
         <div>
-
-
             <section className='sec' id='projetos'>
-                <dialog class='dialogo' id='dialogo'>
-                    <div className='janela'>
-                        <h2 className='titulo-janela'>{getDadosProjetos(projetoSelecionado).titulo}</h2>
-                        <p className='texto-janela'>{getDadosProjetos(projetoSelecionado).texto}
-                        </p>
-                        <div className='divBotao'>
-                            <button onClick={hide} id='esconder' className='esconder'>OK</button>
-                        </div>
-
-                        {/* <Button icon="pi pi-search" severity="success" aria-label="Search" />
-                    <Button label="Submit" link /> */}
-
-                    </div>
-                </dialog>
                 <h1 className='titulo-projetos' id='titu'>Projetos</h1>
                 <div className='main'>
                     <div className='circles' >
                         <button className='bot' onClick={() => { show(); setProjetoSelecionado(1) }} id='mostrar'>
                             <div className='circle' id='RB1'>
                                 <div className='descricao' id='descricao'>
-                                    <p className='descricao-texto'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                                    </p>
+                                    <p className='descricao-texto'>{getDadosProjetos(projetoSelecionado).texto}</p>
                                 </div>
                             </div>
                         </button>
 
                         <button className='bot' onClick={() => { show(); setProjetoSelecionado(2) }} id='mostrar'>
                             <div className='circle' id='Puc'>
+                                <div className='descricao' id='descricao'>
+                                    <p className='descricao-texto'>{getDadosProjetos(projetoSelecionado).texto}</p>
+                                </div>
                             </div>
+                            
                         </button>
+                        
                         <button className='bot' onClick={() => { show(); setProjetoSelecionado(3) }} id='mostrar'>
-                        <div className='circle' id='Puc'>
+                            <div className='circle' id='Puc'>
+                                <div className='descricao' id='descricao'>
+                                    <p className='descricao-texto'>{getDadosProjetos(projetoSelecionado).texto}</p>
+                                </div>
                             </div>
                         </button>
                     </div>
--- src/Conteudo/blocoProjetos/conteudo.js
+++ src/Conteudo/blocoProjetos/conteudo.js
@@ -2,15 +2,15 @@ const listaProjetos = [
     {
         id: 1,
         titulo: 'Meu primeiro projeto',
-        texto: 'Texto do item 1',
-    },    {
+        texto: 'O projeto WordPress para o condomínio RB1 apresenta uma solução integrada com recursos avançados de gestão de usuários, implementando API em Node.js para comunicação eficiente entre o front-end em React.js e o WordPress. Essa abordagem permite uma administração independente. A intergração das APIs (wordpress e minha) permitiram esta flexibilização eficaz da gestão de usuários.',
+    }, {
         id: 2,
         titulo: '222Meu primeiro projeto',
-        texto: 'Texto do item 1',
-    },    {
+        texto: 'Texto do item 2',
+    }, {
         id: 3,
         titulo: '3333Meu primeiro projeto',
-        texto: 'Texto do item 1',
+        texto: 'Texto do item 3',
     },
 
 ]
