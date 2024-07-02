092cb9d2d33ff02aa08e1bbe543802c11c1c8369
diogo939405 <diogo.favilla@gmail.ccom>
2024-07-02 17:08:02 -0300

    terca

--- src/Conteudo/blocoSobreMim/blocoSobreMim.js
+++ src/Conteudo/blocoSobreMim/blocoSobreMim.js
@@ -13,10 +13,10 @@ export default function blocoSobreMim() {
                                 alt=''></img>
                             <br />
                             <div className='curriculo-botão'>
-                            <a href='https://drive.google.com/file/d/14tOQ1ItVZIJBqXxbrOV6N4FboYnRhUxl/view?usp=sharing' target="_blank" rel="noopener noreferrer"> <button className='botao' type='button' >Currículo</button> </a>
-                           <a href='https://drive.google.com/file/d/19K4maJq73i__ByZLnJUH7t1ssOE5J1za/view?usp=sharing' target="_blank" rel="noopener noreferrer"> <button className='botao' type='button' >Resume</button> </a>
+                                <a href='https://drive.google.com/file/d/14tOQ1ItVZIJBqXxbrOV6N4FboYnRhUxl/view?usp=sharing' target="_blank" rel="noopener noreferrer"> <button className='botao' type='button' >Currículo</button> </a>
+                                <a href='https://drive.google.com/file/d/19K4maJq73i__ByZLnJUH7t1ssOE5J1za/view?usp=sharing' target="_blank" rel="noopener noreferrer"> <button className='botao' type='button' >Resume</button> </a>
                             </div>
-                           
+
                         </div>
 
                         <div className='about-text'>
@@ -24,8 +24,11 @@ export default function blocoSobreMim() {
                             <h5>Diogo.Favilla@gmail.com</h5>
                             <h5>Telefone:21-97360-7207</h5>
                             <p>
-                                Meu nome é Diogo Favilla G. Jacintho tenho 22 anos e sou um profissional dedicado e fluente em inglês (formado), recém-formado em Análise e Desenvolvimento de Sistemas.
-                                Nesta curta caminhada obtive sucessos como por exemplo Site da Associação da Polícia Federal, Loja virtual de artigos históricos entre outros nos quais tive co-participação. Fiz Diversos cursos com certificação e vários projetos pessoais (como este portfolio) que agregam valor a minha experiência.                            </p>
+                                Olá! Meu nome é Diogo Favilla Gonçalves Jacintho.
+                                Sou formado em Análise e Desenvolvimento de Sistemas e em Língua Inglesa pelo Brasas. Tenho paixão por tecnologia e aprendizado constante.
+                                Foco principalmente em frontend, criando interfaces intuitivas e atraentes, mas também possuo bom conhecimento em backend. Sou determinado, trabalho bem em equipe e já tive papéis de liderança.
+                                Participei de projetos que utilizam CRUD integrado a um site do WordPress, um site de pagamentos com Mercado Pago API e um aplicativo mobile utilizando Flutter. Sempre em busca de novos desafios, estou disponível para mudanças e novas oportunidades.
+                                Obrigado por visitar meu portfólio! Sinta-se à vontade para explorar meus projetos e entrar em contato.</p>
 
                         </div>
                     </div>
